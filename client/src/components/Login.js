c312950a1469d8c7135e18e8fb756c724559241b
Adam <ruffneradam@aol.com>
2023-05-01 12:48:11 -0400

    react app initialized, routes, login rendering

---
+++ client/src/components/Login.js
@@ -0,0 +1,52 @@
+import { React, useState } from 'react';
+import { useNavigate, Link, useSearchParams } from 'react-router-dom'; 
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSumbit = (e) => {
+        if (username.trim() && password.trim()) {
+            e.preventDefault();
+            console.log({username, password})
+            setUsername('');
+            setPassword('');
+        }
+    };
+
+    return (
+        <main className='login'>
+            <form className='login-form'>
+                <h2 className='login-title'>Log into your account</h2>
+                <label htmlFor='username'>Username</label>
+                <input 
+                    id='username'
+                    name='username'
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className='username'
+                />
+                <label htmlFor='password'>Password</label>
+                <input 
+                    id='password'
+                    name='password'
+                    type='password'
+                    value={password}
+                    onChange={(e => setPassword(e.target.value))}
+                    className='password'
+                />
+                <button className='login-button'>LOG IN</button>
+                <p>
+                    Don't have an account? {''}
+                    <Link className='link' to='/register'>
+                        Create One
+                    </Link>
+                </p>
+            </form>
+        </main>
+    )
+}
+
+export default Login;
